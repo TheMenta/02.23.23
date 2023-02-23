95c51759c0db14fd5c511705b33b93c33f9ac40d
thementa <thementaaa@gmail.com>
2023-02-23 15:51:29 +0100

    metodus v3 (gyok)

--- Osszefoglalo/src/Main.java
+++ Osszefoglalo/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         kiir(String.format("Az első 10 szám összege: %d", elso10SzamOsszege()));
         kiir(String.format("%d + %d = %d", 3, 4, osszead(3, 4)));
         kiir(String.format("%d + %d + %d + %d = %d\n", 1, 2, 3, 4, osszead(osszead(1, 2), osszead(3, 4))));
+        kiir(String.format("%d + %d + %d gyöke: %f\n", 3, 4, 2,
     }
 
     static int elso10SzamOsszege()
