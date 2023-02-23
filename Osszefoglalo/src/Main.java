0e03f820503f25cbe9caf1584deed385e1b54b72
thementa <thementaaa@gmail.com>
2023-02-23 15:22:53 +0100

    javitas

--- Osszefoglalo/src/Main.java
+++ Osszefoglalo/src/Main.java
@@ -1,13 +1,27 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        static int 10SzamOsszege() {
-            int osszeg = 0;
-            for (int i = 0; i < 10; i++)
-            {
-                osszeg += i;
-            }
-            return osszeg;
+    public static void main(String[] args)
+    {
+        kiir(String.format("Az első 10 szám összege: %d", elso10SzamOsszege()));
+        kiir(String.format("%d + %d = %d", 3, 4, osszead(3, 4)));
+    }
+
+    static int elso10SzamOsszege()
+    {
+        int osszeg = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            osszeg += i;
         }
+        return osszeg;
+    }
+
+    static int osszead(int a, int b)
+    {
+        return a + b;
+    }
+
+    static void kiir(String szoveg)
+    {
+        System.out.println(szoveg);
     }
 }
