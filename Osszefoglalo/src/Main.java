850d0d41917844a2697fa46e02271866a70a81cd
thementa <thementaaa@gmail.com>
2023-02-23 15:11:13 +0100

    elso kommit

---
+++ Osszefoglalo/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        static int 10SzamOsszege() {
+            int osszeg = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                osszeg += i;
+            }
+            return osszeg;
+        }
+    }
+}
