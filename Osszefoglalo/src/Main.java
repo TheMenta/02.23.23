fd20230c2e59ff92961b74444251f23acd0fe3bc
thementa <thementaaa@gmail.com>
2023-02-23 16:03:26 +0100

    metodus v4 feladatokra bontas

--- Osszefoglalo/src/Main.java
+++ Osszefoglalo/src/Main.java
@@ -1,10 +1,49 @@
 public class Main {
     public static void main(String[] args)
     {
-        kiir(String.format("Az első 10 szám összege: %d", elso10SzamOsszege()));
-        kiir(String.format("%d + %d = %d", 3, 4, osszead(3, 4)));
-        kiir(String.format("%d + %d + %d + %d = %d\n", 1, 2, 3, 4, osszead(osszead(1, 2), osszead(3, 4))));
-        kiir(String.format("%d + %d + %d gyöke: %f\n", 3, 4, 2,
+        feladatok();
+    }
+
+    static void feladatok()
+    {
+        feladat1();
+        feladat2();
+        feladat3();
+        feladat4();
+    }
+
+    static void feladat1()
+    {
+        int osszeg = elso10SzamOsszege();
+        kiir(String.format("Az első 10 szám összege: %d\n", osszeg));
+    }
+
+    static void feladat2()
+    {
+        int a = 3;
+        int b = 4;
+        int osszeg = osszead(a, b);
+        kiir(String.format("%d + %d = %d\n", a, b, osszeg));
+    }
+
+    static void feladat3()
+    {
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        int osszeg = osszead(osszead(1, 2), osszead(3, 4));
+        kiir(String.format("%d + %d + %d + %d = %d\n", a, b, c, d, osszeg));
+    }
+
+    static void feladat4()
+    {
+        int a = 3;
+        int b = 4;
+        int c = 2;
+        int osszeg = osszead(3, osszead(4, 2));
+        double gyok = Math.sqrt(osszeg);
+        kiir(String.format("%d + %d + %d gyöke: %f\n", a, b, c, gyok));
     }
 
     static int elso10SzamOsszege()
@@ -24,6 +63,6 @@ public class Main {
 
     static void kiir(String szoveg)
     {
-        System.out.println(szoveg);
+        System.out.print(szoveg);
     }
 }
